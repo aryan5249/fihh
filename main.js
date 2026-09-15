77edc0209f25ff1d11747afc67739bce1d1ac7e8
Aryan Sahu <aryansahuofficial@gmail.com>
2026-09-16 00:41:35 +0530

    inicommit

---
+++ main.js
@@ -0,0 +1,63 @@
+class Game{
+    constructor(canvas, context){
+        this.canvas = canvas;
+        this.ctx = context;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        this.baseHeight = 720;
+        this.ratio = this.height / this.baseHeight;
+        this.player = new Player(this);
+        this.gravity;
+
+        this.resize(window.innerWidth, window.innerHeight);
+
+        window.addEventListener('resize', e => {
+            this.resize(e.currentTarget.innerWidth, e.currentTarget.innerHeight );
+        });
+
+        this.canvas.addEventListener('mousedown', e => {
+            this.player.flap();
+        });
+        window.addEventListener('keydown', e =>{
+            if(e.key ===' ' || e.key=== 'Enter' || e.key ==='w' || e.key ==='W' || e.key ==='ArrowUp'){
+                this.player.flap();
+            }
+        });
+        this.canvas.addEventListener('touchstart', e=> {
+            this.player.flap();
+        });
+    }
+    resize(width, height){
+        this.canvas.width = width;
+        this.canvas.height = height;
+        this.ctx.fillStyle = 'red';
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        this.ratio = this.height / this.baseHeight;
+
+        this.gravity = 0.15 * this.ratio;
+
+        this.player.resize();
+    } 
+    render(){
+        this.player.update();
+        this.player.draw();
+    }
+}
+
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 720;
+    canvas.height =720;
+
+    const game = new Game(canvas, ctx);
+    game.render();
+
+    function animate(){
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        game.render();
+        requestAnimationFrame(animate);
+    }
+    animate();
+});
